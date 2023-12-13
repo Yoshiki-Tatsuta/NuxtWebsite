33b6830dc5f46dc9a4b75c8753590fd928a3fc5d
龍田 佳樹 <yo-tatsuta@a.tgl.co.jp>
2023-12-13 14:30:57 +0900

    202321213

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,13 @@
 <template lang="pug">
 v-container(fill-height)
   v-row
-    v-col(class="text-center")
+    v-col(class="text-center" cols="10")
       h1 トップページ
       br
       p これからいろんなプログラミングに関する記事をここに書いていく予定
+    v-col(class="text-center" cols="2")
+      h2 広告
+      p ここに広告を表示
 </template>
 
 <script lang="ts">
